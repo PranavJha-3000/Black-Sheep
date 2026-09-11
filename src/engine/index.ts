194ebeff5c23dee4a5761b10a5873b4ebc44688d
Pranav Jha <pranavstark215047@gmail.com>
2026-09-11 23:26:07 +0530

    Export engine public API

---
+++ src/engine/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Black Sheep - engine public API.
+ * Re-export everything the UI/store layer is allowed to consume.
+ */
+
+export * from './types';
+export * from './market';
+export * from './portfolio';
+export * from './events';
